e790ddfe140c3eb2416080b7d59bd517dc47c80d
Nikolay Haustov <nhaustov@gmail.com>
2014-12-23 20:33:10 +0300

    Set debug build in example script.

--- cmake_linux.sh
+++ cmake_linux.sh
@@ -1,3 +1,3 @@
 mkdir build
 cd build
-cmake .. -DHSAIL-Tools-PATH=/srv/HSAIL-Tools -DHSA-Runtime-Inc-PATH=/srv/hsa/drivers/hsa/runtime/inc -DHSA-Runtime-Ext-PATH=/srv/hsa/drivers/hsa/compiler/finalizer/Interface && make 
+cmake -DCMAKE_BUILD_TYPE=Debug .. -DHSAIL-Tools-PATH=/srv/HSAIL-Tools -DHSA-Runtime-Inc-PATH=/srv/hsa/drivers/hsa/runtime/inc -DHSA-Runtime-Ext-PATH=/srv/hsa/drivers/hsa/compiler/finalizer/Interface && make 
