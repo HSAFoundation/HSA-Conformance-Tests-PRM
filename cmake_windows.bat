5d9e97a00718d67f6e675efb33f636b1d54cdbba
Nikolay Haustov <nhaustov@gmail.com>
2014-12-23 17:50:09 +0300

    CMake build changes for Windows build.

---
+++ cmake_windows.bat
@@ -0,0 +1,3 @@
+mkdir build
+cd build
+cmake .. -DHSAIL-Tools-PATH=C:/ws/git/HSAIL-Tools -DHSA-Runtime-Inc-PATH=C:/ws/hsa/drivers/hsa/runtime/inc -DHSA-Runtime-Ext-PATH=C:/ws/hsa/drivers/hsa/compiler/finalizer/Interface
