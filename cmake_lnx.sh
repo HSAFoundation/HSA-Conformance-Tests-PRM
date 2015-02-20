f80cd36fccaf5f8cd1b0b15f9bed0b651d7a970e
Nikolay Haustov <nhaustov@gmail.com>
2015-02-20 16:38:25 +0300

    Fix build on Linux

--- cmake_lnx.sh
+++ cmake_lnx.sh
@@ -2,34 +2,34 @@
 
 set -e
 
-export HSAIL_TOOLS=H:/ws/git/HSAIL-Tools
-export HSA_RUNTIME_INC=H:/ws/hsa/drivers/hsa/runtime/inc
-export HSA_RUNTIME_EXT_INC=H:/ws/hsa/drivers/hsa/compiler/finalizer/Interface
-export OCL_PATH=H:/ws/hsa/drivers/opencl/dist/windows/debug
-export ORCA_OPTS=-DOCL-Path=%OCL_PATH% -DENABLE_HEXL_ORCA=1
+export HSAIL_TOOLS=/srv/git/HSAIL-Tools
+export HSA_RUNTIME_INC=/srv/hsa/drivers/hsa/runtime/inc
+export HSA_RUNTIME_EXT_INC=/srv/hsa/drivers/hsa/compiler/finalizer/Interface
+export OCL_PATH=/srv/ws/hsa/drivers/opencl/dist/windows/debug
+export ORCA_OPTS="-DOCL-Path=%OCL_PATH% -DENABLE_HEXL_ORCA=1"
 
 export BASIC_OPTS="-DHSAIL-Tools-PATH=$HSAIL_TOOLS -DHSA-Runtime-Inc-PATH=$HSA_RUNTIME_INC -DHSA-Runtime-Ext-Inc-PATH=$HSA_RUNTIME_EXT_INC"
 
 mkdir -p build/lnx32
 cd build/lnx32
 export EXTRA_CFLAGS=-m32
-cmake -G "Unix Makefiles" $BASIC_OPTS -DEXTRA_CFLAGS=$EXTRA_CFLAGS "$@" ../..
+cmake -G "Unix Makefiles" $BASIC_OPTS -DHSAIL-Tools-Subdir=lnx32 -DEXTRA_CFLAGS=$EXTRA_CFLAGS "$@" ../..
 cd ../..
 
 mkdir -p build/lnx64
 cd build/lnx64
 export EXTRA_CFLAGS=-m64
-cmake -G "Unix Makefiles" $BASIC_OPTS -DEXTRA_CFLAGS=$EXTRA_CFLAGS "$@" ../..
+cmake -G "Unix Makefiles" $BASIC_OPTS -DHSAIL-Tools-Subdir=lnx64 -DEXTRA_CFLAGS=$EXTRA_CFLAGS "$@" ../..
 cd ../..
 
 mkdir -p build/lnx32_orca
 cd build/lnx32_orca
 export EXTRA_CFLAGS=-m32
-cmake -G "Unix Makefiles" $BASIC_OPTS -DEXTRA_CFLAGS=$EXTRA_CFLAGS $ORCA_OPTS "$@" ../..
+cmake -G "Unix Makefiles" $BASIC_OPTS -DHSAIL-Tools-Subdir=lnx32 -DEXTRA_CFLAGS=$EXTRA_CFLAGS $ORCA_OPTS "$@" ../..
 cd ../..
 
 mkdir -p build/lnx64_orca
 cd build/lnx64_orca
 export EXTRA_CFLAGS=-m64
-cmake -G "Unix Makefiles" $BASIC_OPTS -DEXTRA_CFLAGS=$EXTRA_CFLAGS $ORCA_OPTS "$@" ../..
+cmake -G "Unix Makefiles" $BASIC_OPTS -DHSAIL-Tools-Subdir=lnx64 -DEXTRA_CFLAGS=$EXTRA_CFLAGS $ORCA_OPTS "$@" ../..
 cd ../..
