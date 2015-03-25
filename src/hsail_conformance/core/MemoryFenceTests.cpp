4ad0aaa43cdbfc7f5d848093c2da25a3a58423ec
Evgeny Mankov <Evgeny.Mankov@gmail.com>
2015-03-25 16:19:24 +0300

    6.9. Memory fences. Compound tests on fencing 2 different global/group vars by 1 memfence operation (+480)

--- src/hsail_conformance/core/MemoryFenceTests.cpp
+++ src/hsail_conformance/core/MemoryFenceTests.cpp
@@ -31,7 +31,7 @@ class MemoryFenceTest : public Test {
 public:
   MemoryFenceTest(Grid geometry_, BrigType type_, BrigMemoryOrder memoryOrder1_, BrigMemoryOrder memoryOrder2_, BrigSegment segment_, BrigMemoryScope memoryScope_)
   : Test(KERNEL, geometry_), type(type_), memoryOrder1(memoryOrder1_), memoryOrder2(memoryOrder2_), segment(segment_), memoryScope(memoryScope_),
-    initialValue(0) {}
+    initialValue(0), s_label_skip_store("@skip_store"), s_label_skip_memfence("@skip_memfence") {}
 
   void Name(std::ostream& out) const {
     out << segment2str(segment) << "_" << type2str(type) << "/"
@@ -47,8 +47,9 @@ protected:
   BrigSegment segment;
   BrigMemoryScope memoryScope;
   int64_t initialValue;
+  std::string s_label_skip_store;
+  std::string s_label_skip_memfence;
   DirectiveVariable globalVar;
-  OperandAddress globalVarAddr;
   Buffer input;
   TypedReg inputReg;
 
@@ -91,14 +92,14 @@ protected:
       globalVar.init() = be.Immed(type, initialValue);
   }
 
-  virtual void EmitInstrToTest(BrigOpcode opcode, TypedReg reg) {
-    globalVarAddr = be.Address(globalVar);
+  virtual void EmitInstrToTest(BrigOpcode opcode, BrigSegment seg, BrigType t, TypedReg reg, DirectiveVariable var) {
+    OperandAddress globalVarAddr = be.Address(var);
     switch (opcode) {
       case BRIG_OPCODE_LD:
-        be.EmitLoad(segment, type, reg->Reg(), globalVarAddr);
+        be.EmitLoad(seg, t, reg->Reg(), globalVarAddr);
         break;
       case BRIG_OPCODE_ST:
-        be.EmitStore(segment, type, reg->Reg(), globalVarAddr);
+        be.EmitStore(seg, t, reg->Reg(), globalVarAddr);
         break;
       default: assert(false); break;
     }
@@ -110,12 +111,10 @@ protected:
     input->EmitLoadData(inputReg);
     TypedReg wiID = be.EmitWorkitemFlatAbsId(true);
     TypedReg cReg = be.AddTReg(BRIG_TYPE_B1);
-    SRef s_label_skip_store = "@skip_store";
-    SRef s_label_skip_memfence = "@skip_memfence";
     be.EmitCmp(cReg->Reg(), wiID, be.Immed(wiID->Type(), 1), BRIG_COMPARE_NE);
     be.EmitCbr(cReg, s_label_skip_store);
 
-    EmitInstrToTest(BRIG_OPCODE_ST, inputReg);
+    EmitInstrToTest(BRIG_OPCODE_ST, segment, type, inputReg, globalVar);
     be.EmitMemfence(memoryOrder1, memoryScope, memoryScope, BRIG_MEMORY_SCOPE_NONE);
     be.EmitBr(s_label_skip_memfence);
     be.EmitLabel(s_label_skip_store);
@@ -123,19 +122,19 @@ protected:
     be.EmitMemfence(memoryOrder2, memoryScope, memoryScope, BRIG_MEMORY_SCOPE_NONE);
     be.EmitLabel(s_label_skip_memfence);
 
-    EmitInstrToTest(BRIG_OPCODE_LD, result);
+    EmitInstrToTest(BRIG_OPCODE_LD, segment, type, result, globalVar);
     return result;
   }
 };
 
-/*
+
 class MemoryFenceCompoundTest : public MemoryFenceTest {
 protected:
   BrigType type2;
   BrigSegment segment2;
   Buffer input2;
+  TypedReg inputReg2;
   DirectiveVariable globalVar2;
-  OperandAddress globalVarAddr2;
 
 public:
   MemoryFenceCompoundTest(Grid geometry_, BrigType type_, BrigType type2_, BrigMemoryOrder memoryOrder1_, BrigMemoryOrder memoryOrder2_, BrigSegment segment_, BrigSegment segment2_, BrigMemoryScope memoryScope_)
@@ -150,7 +149,15 @@ public:
 
   BrigType ResultType() const { return type; }
 
+  BrigType ResultType2() const { return type2; }
+
   bool IsValid() const {
+    if (type != type2)
+      return false;
+    //if (isFloatType(type) || isFloatType(type2))
+    //  return false;
+    if (16 == getBrigTypeNumBits(type) || 16 == getBrigTypeNumBits(type2))
+      return false;
     if ((BRIG_SEGMENT_GROUP == segment || BRIG_SEGMENT_GROUP == segment2) && geometry->WorkgroupSize() != geometry->GridSize())
       return false;
     if (BRIG_MEMORY_ORDER_SC_ACQUIRE == memoryOrder1)
@@ -188,29 +195,58 @@ public:
       globalVar2.init() = be.Immed(type2, initialValue);
   }
 
-  void EmitInstrToTest(BrigOpcode opcode) {
-    MemoryFenceTest::EmitInstrToTest(opcode);
-    globalVarAddr2 = be.Address(globalVar2);
-    switch (opcode) {
-      case BRIG_OPCODE_LD:
-        be.EmitLoad(segment2, type2, destReg->Reg(), globalVarAddr2);
-        break;
-      case BRIG_OPCODE_ST:
-        be.EmitStore(segment2, type2, inputReg->Reg(), globalVarAddr2);
-        break;
-      default: assert(false); break;
+  TypedReg Result() {
+    TypedReg result = be.AddTReg(ResultType());
+    TypedReg result2 = be.AddTReg(ResultType2());
+    inputReg = be.AddTReg(type);
+    inputReg2 = be.AddTReg(type2);
+    input->EmitLoadData(inputReg);
+    input2->EmitLoadData(inputReg2);
+    TypedReg wiID = be.EmitWorkitemFlatAbsId(true);
+    TypedReg cReg = be.AddTReg(BRIG_TYPE_B1);
+    be.EmitCmp(cReg->Reg(), wiID, be.Immed(wiID->Type(), 1), BRIG_COMPARE_NE);
+    be.EmitCbr(cReg, s_label_skip_store);
+
+    EmitInstrToTest(BRIG_OPCODE_ST, segment, type, inputReg, globalVar);
+    EmitInstrToTest(BRIG_OPCODE_ST, segment2, type2, inputReg2, globalVar2);
+    be.EmitMemfence(memoryOrder1, memoryScope, memoryScope, BRIG_MEMORY_SCOPE_NONE);
+    be.EmitBr(s_label_skip_memfence);
+    be.EmitLabel(s_label_skip_store);
+
+    be.EmitMemfence(memoryOrder2, memoryScope, memoryScope, BRIG_MEMORY_SCOPE_NONE);
+    be.EmitLabel(s_label_skip_memfence);
+
+    EmitInstrToTest(BRIG_OPCODE_LD, segment, type, result, globalVar);
+    EmitInstrToTest(BRIG_OPCODE_LD, segment2, type2, result2, globalVar2);
+    if (type != type2) {
+      if (getBrigTypeNumBits(type) == getBrigTypeNumBits(type2) && isIntType(type) && isIntType(type2)) {
+        be.EmitArith(BRIG_OPCODE_ADD, result, result->Reg(), result2->Reg());
+      } else {
+        BrigRound round = BRIG_ROUND_NONE;
+        if (isIntType(type) && isFloatType(type2)) {
+          round = BRIG_ROUND_INTEGER_ZERO;
+        }
+        if (isIntType(type2) && isFloatType(type)) {
+          round = BRIG_ROUND_FLOAT_ZERO;
+        }
+        TypedReg result1 = be.AddTReg(ResultType());
+        be.EmitCvt(result1, result2, round);
+        be.EmitArith(BRIG_OPCODE_ADD, result, result->Reg(), result1->Reg());
+      }
+    } else {
+      be.EmitArith(BRIG_OPCODE_ADD, result, result->Reg(), result2->Reg());
     }
+    return result;
   }
 
 };
-*/
 
 void MemoryFenceTests::Iterate(TestSpecIterator& it)
 {
   CoreConfig* cc = CoreConfig::Get(context);
   Arena* ap = cc->Ap();
   TestForEach<MemoryFenceTest>(ap, it, "memfence/basic", cc->Grids().MemfenceSet(), cc->Types().Memfence(), cc->Memory().MemfenceMemoryOrders(), cc->Memory().MemfenceMemoryOrders(), cc->Memory().MemfenceSegments(), cc->Memory().MemfenceMemoryScopes());
-//  TestForEach<MemoryFenceCompoundTest>(ap, it, "memfence/compound", cc->Grids().MemfenceSet(), cc->Types().Memfence(), cc->Types().Memfence(), cc->Memory().MemfenceMemoryOrders(), cc->Memory().MemfenceMemoryOrders(), cc->Memory().MemfenceSegments(), cc->Memory().MemfenceSegments(), cc->Memory().MemfenceMemoryScopes());
+  TestForEach<MemoryFenceCompoundTest>(ap, it, "memfence/compound", cc->Grids().MemfenceSet(), cc->Types().Memfence(), cc->Types().Memfence(), cc->Memory().MemfenceMemoryOrders(), cc->Memory().MemfenceMemoryOrders(), cc->Memory().MemfenceSegments(), cc->Memory().MemfenceSegments(), cc->Memory().MemfenceMemoryScopes());
 }
 
 }
