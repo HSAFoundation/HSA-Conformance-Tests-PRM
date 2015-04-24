b93d2213cae550b5725ff5a503175fc1b4ca1d17
Andrey Kasaurov <Andrey.Kasaurov@gmail.com>
2015-04-24 13:47:09 +0300

    License update

--- src/hsail_conformance/image/ImageLdTests.cpp
+++ src/hsail_conformance/image/ImageLdTests.cpp
@@ -1,5 +1,5 @@
 ﻿/*
-   Copyright 2014 Heterogeneous System Architecture (HSA) Foundation
+   Copyright 2014-2015 Heterogeneous System Architecture (HSA) Foundation
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
